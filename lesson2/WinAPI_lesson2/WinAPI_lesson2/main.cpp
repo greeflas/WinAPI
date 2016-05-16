3f259073e10f39f2882e64a80cda85b5a3170008
student <vldmr.kuprienko@gmail.com>
2016-05-16 10:20:15 +0300

    example 1

--- lesson2/WinAPI_lesson2/WinAPI_lesson2/main.cpp
+++ lesson2/WinAPI_lesson2/WinAPI_lesson2/main.cpp
@@ -4,7 +4,7 @@
 #include "resource.h"
 
 // Global variables
-
+HWND hStartBtn, hStopBtn;
 
 BOOL CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
 
@@ -20,13 +20,30 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
 BOOL CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
+	static int x = 0;
+
 	switch (message)
 	{
 	case WM_INITDIALOG:
-		SetTimer(hDlg, 1, 2000, NULL);
+		//SetTimer(hDlg, 1, 2000, NULL); // Example 1
+		hStartBtn = GetDlgItem(hDlg, IDC_BUTTON1);
+		hStopBtn = GetDlgItem(hDlg, IDC_BUTTON2);
 		break;
 	case WM_TIMER:
-		MessageBox(hDlg, L"ку-ку", L"Information", MB_OK | MB_ICONINFORMATION);
+		//MessageBox(hDlg, L"ku-ku", L"Information", MB_OK | MB_ICONINFORMATION); // Example 1
+		MoveWindow(hDlg, 0 + x, 100, 250, 130, TRUE);
+		x += 10;
+		break;
+	case WM_COMMAND:
+		switch (wParam)
+		{
+		case IDC_BUTTON1:
+			SetTimer(hDlg, 1, 20, NULL);
+			break;
+		case IDC_BUTTON2:
+			KillTimer(hDlg, 1);
+			break;
+		}
 		break;
 	case WM_CLOSE:
 		EndDialog(hDlg, 0);
