fbf3443c56072d6d00ac386d50ec8f34ed37d0a5
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-05-18 11:23:53 +0300

    radio button test

--- lesson3/WinAPI_lesson3/WinAPI_lesson3/main.cpp
+++ lesson3/WinAPI_lesson3/WinAPI_lesson3/main.cpp
@@ -23,9 +23,13 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
 BOOL CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
+	TCHAR buf[512] {0};
+	int flag(0);
+
 	switch (message)
 	{
 	case WM_INITDIALOG:
+		// Handlers
 		hEditSum = GetDlgItem(hDlg, IDC_EDIT1);
 		hEditResult = GetDlgItem(hDlg, IDC_EDIT2);
 		hRadioUSD = GetDlgItem(hDlg, IDC_RADIO1);
@@ -38,7 +42,31 @@ BOOL CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 		hBtnCalc = GetDlgItem(hDlg, IDC_BUTTON1);
 		hBtnReset = GetDlgItem(hDlg, IDC_BUTTON2);
 
+		// Disable windows
 		EnableWindow(hEditResult, FALSE);
+		EnableWindow(hBtnReset, FALSE);
+
+		// Set default values
+		SendMessage(hRadioUSD, BM_SETCHECK, (WPARAM)BST_CHECKED, 0);
+		SendMessage(hRadioBuy, BM_SETCHECK, (WPARAM)BST_CHECKED, 0);
+		break;
+	case WM_COMMAND:
+		switch (wParam)
+		{
+		case IDC_BUTTON1: // calculate button
+			EnableWindow(hBtnReset, TRUE);
+
+			flag = SendMessage(hRadioUSD, BM_GETCHECK, 0, 0);
+			if (flag == BST_CHECKED)
+			{
+				lstrcpy(buf, L"USD"); // strcpy() for unicode
+			}
+			MessageBox(hDlg, buf, L"Flag", MB_OK);
+			break;
+		case IDC_BUTTON2: // reset button
+			EnableWindow(hBtnReset, FALSE);
+			break;
+		}
 		break;
 	case WM_CLOSE:
 			EndDialog(hDlg, 0);
