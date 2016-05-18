e057e7ca36f1ffcef86e1328b14affe1662ed479
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-05-18 10:13:04 +0300

    case init dialog

--- lesson3/WinAPI_lesson3/WinAPI_lesson3/main.cpp
+++ lesson3/WinAPI_lesson3/WinAPI_lesson3/main.cpp
@@ -4,7 +4,10 @@
 #include "resource.h"
 
 // Global variables
-
+HWND hEditSum, hEditResult;
+HWND hRadioUSD, hRadioEUR, hRadioRUB, hRadioBuy, hRadioSale;
+HWND hCheckPrint, hCheckSave;
+HWND hBtnCalc, hBtnReset;
 
 BOOL CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
 
@@ -23,7 +26,19 @@ BOOL CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 	switch (message)
 	{
 	case WM_INITDIALOG:
+		hEditSum = GetDlgItem(hDlg, IDC_EDIT1);
+		hEditResult = GetDlgItem(hDlg, IDC_EDIT2);
+		hRadioUSD = GetDlgItem(hDlg, IDC_RADIO1);
+		hRadioEUR = GetDlgItem(hDlg, IDC_RADIO2);
+		hRadioRUB = GetDlgItem(hDlg, IDC_RADIO3);
+		hRadioBuy = GetDlgItem(hDlg, IDC_RADIO4);
+		hRadioSale = GetDlgItem(hDlg, IDC_RADIO5);
+		hCheckPrint = GetDlgItem(hDlg, IDC_CHECK1);
+		hCheckSave = GetDlgItem(hDlg, IDC_CHECK2);
+		hBtnCalc = GetDlgItem(hDlg, IDC_BUTTON1);
+		hBtnReset = GetDlgItem(hDlg, IDC_BUTTON2);
 
+		EnableWindow(hEditResult, FALSE);
 		break;
 	case WM_CLOSE:
 			EndDialog(hDlg, 0);
