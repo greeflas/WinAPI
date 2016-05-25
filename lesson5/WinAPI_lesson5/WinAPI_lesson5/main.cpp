13573c976e8c93dfe31ab0081854eb3c9ae39f8e
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-05-25 11:55:12 +0300

    add goods to basket

--- lesson5/WinAPI_lesson5/WinAPI_lesson5/main.cpp
+++ lesson5/WinAPI_lesson5/WinAPI_lesson5/main.cpp
@@ -27,9 +27,9 @@ const TCHAR CAT3[] = L"Компьютеры для работы и учёбы";
 // goods
 const int GOODS_NUM = 3;
 const TCHAR *CAT_NOTEBOOKS[GOODS_NUM] = {
-	L"Dell Inspiron 5558, Цена - 19 900грн",
-	L"Dell Inspiron 3543, Цена - 13 799грн",
-	L"Dell Inspiron 3542, Цена - 7 099грн"
+	L"Dell Inspiron 5558, 19 900грн",
+	L"Dell Inspiron 3543, 13 799грн",
+	L"Dell Inspiron 3542, 7 099грн"
 };
 
 BOOL CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
@@ -46,6 +46,9 @@ BOOL CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	const int BUF_SIZE = 1024;
 	TCHAR buf[BUF_SIZE] { 0 };
+
+	int list_items(0);
+	int index(0);
 
 	switch (message)
 	{
@@ -72,10 +75,22 @@ BOOL CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 		switch (wParam)
 		{
 		case IDC_BTN_ADD:
+			list_items = SendMessage(hListGoods, LB_GETCOUNT, 0, 0);
+			if (list_items)
+			{
+				index = SendMessage(hListGoods, LB_GETCURSEL, 0, 0);
+				SendMessage(hListGoods, LB_GETTEXT, index, (LPARAM)buf);
+				SendMessage(hListBasket, LB_ADDSTRING, 0, (LPARAM)buf);
+			}
+			else
+			{
+				MessageBox(hDlg, L"List is empty!", L"Error", MB_OK | MB_ICONERROR);
+			}
 			break;
 		case IDC_BTN_DEL:
 			break;
 		case IDC_BTN_CLEAR:
+			SendMessage(hListBasket, LB_RESETCONTENT, 0, 0);
 			break;
 		}
 
@@ -84,14 +99,12 @@ BOOL CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 		{
 			SendMessage(hListGoods, LB_RESETCONTENT, 0, 0);
 
-			int cat = SendMessage(hComboCat, CB_GETCURSEL, 0, 0);
-			switch (cat)
+			int category = SendMessage(hComboCat, CB_GETCURSEL, 0, 0);
+			switch (category)
 			{
 			case Category::NOTEBOOKS:
 				for (size_t i = 0; i < GOODS_NUM; ++i)
-				{
 					SendMessage(hListGoods, LB_ADDSTRING, 0, (LPARAM)CAT_NOTEBOOKS[i]);
-				}
 				break;
 			}
 		}
